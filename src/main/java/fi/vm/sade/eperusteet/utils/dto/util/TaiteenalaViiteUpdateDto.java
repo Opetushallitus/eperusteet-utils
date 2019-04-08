3418dd957f5e03f5d1d151298ca0ebf4dfa31d32
Sauli Anto <sauli.anto@gmail.com>
2019-04-08 11:06:14 +0300

    Revert "Siirretty DTO:t perustepalvelusta"
    This reverts commit 837da9df

--- src/main/java/fi/vm/sade/eperusteet/utils/dto/util/TaiteenalaViiteUpdateDto.java
+++
@@ -1,6 +0,0 @@
-package fi.vm.sade.eperusteet.utils.dto.util;
-
-import fi.vm.sade.eperusteet.utils.dto.yl.TaiteenalaDto;
-
-public class TaiteenalaViiteUpdateDto extends UpdateDto<TaiteenalaDto> {
-}
--- src/main/java/fi/vm/sade/eperusteet/utils/dto/yl/AIPEHasId.java
+++
@@ -1,6 +0,0 @@
-package fi.vm.sade.eperusteet.utils.dto.yl;
-
-
-public interface AIPEHasId {
-    Long getId();
-}
